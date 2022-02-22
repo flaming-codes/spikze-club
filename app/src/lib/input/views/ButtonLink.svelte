fb2b2adf624f96a98dcdbbe54b497eccd0ca7895
Thomas Schönmann <tom@flaming.codes>
2022-02-22 19:52:14 +0000

    wip

--- app/src/lib/input/views/ButtonLink.svelte
+++ app/src/lib/input/views/ButtonLink.svelte
@@ -14,6 +14,11 @@
   export let alt: string;
   export let targetVariant: "external" | "section" = undefined;
   export let variant: "outline" | "bare" = undefined;
+
+  /*
+  rel={targetVariant === "external" && "external"}
+  target={targetVariant === "external" && "_blank"}
+  */
 </script>
 
 <a
@@ -21,8 +26,6 @@
   on:mouseleave
   {href}
   aria-label={alt}
-  rel={targetVariant === "external" && "external"}
-  target={targetVariant === "external" && "_blank"}
   class={clsx(
     `${buttonBaseMixin} ${buttonLinkBaseAnimationMixin} inline-flex items-center justify-center gap-2`,
     {
