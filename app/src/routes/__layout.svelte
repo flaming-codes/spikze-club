c3ee6e0b4cfccec6d0ad2a353ab6c5023aaca9cd
Thomas Schönmann <tom@flaming.codes>
2022-03-17 19:55:02 +0000

    Update .env

--- app/src/routes/__layout.svelte
+++ app/src/routes/__layout.svelte
@@ -5,8 +5,6 @@
   import PageLayout from "$lib/layout/views/PageLayout.svelte";
   import { dev } from "$app/env";
 
-  console.log("!import.meta.env.VITE_MSW_ENABLED", import.meta.env.VITE_MSW_ENABLED === "true");
-
   const isMswEnabled = dev && import.meta.env.VITE_MSW_ENABLED === "true";
   // Flag to defer rendering of components
   // until certain criteria are met on dev,
@@ -14,7 +12,6 @@
   let isReady = !isMswEnabled;
 
   if (isMswEnabled) {
-    console.log("isMswEnabled", isMswEnabled);
     import("$msw")
       .then((res) => res.inject())
       .then(() => (isReady = true));
